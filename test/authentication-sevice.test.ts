f80e5cc0c080ab6a598118319a73a3f1981b259f
Mamadou <sallmahdou1@gmail.com>
2024-04-24 15:23:29 +0200

    feat: initialise cdk typescript project

---
+++ test/authentication-sevice.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AuthenticationSevice from '../lib/authentication-sevice-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/authentication-sevice-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AuthenticationSevice.AuthenticationSeviceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
